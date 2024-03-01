dce8fdfc005c2d58a8b4283146b41b8f420564f2
Chris Lansing <cslansin@gmail.com>
2024-03-01 15:35:40 -0500

    add firebase client and server

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,20 @@
 /// <reference types="astro/client" />
 type Runtime = import("@astrojs/cloudflare").DirectoryRuntime<Env>;
+
 declare namespace App {
 	interface Locals extends Runtime {}
+}
+
+interface ImportMetaEnv {
+	readonly FIREBASE_PUBLIC_API_KEY: string;
+	readonly FIREBASE_AUTH_DOMAIN: string;
+	readonly FIREBASE_PROJECT_ID: string;
+	readonly FIREBASE_STORAGE_BUCKET: string;
+	readonly FIREBASE_MESSAGING_SENDER_ID: string;
+	readonly FIREBASE_APP_ID: string;
+	readonly FIREBASE_MEASUREMENT_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }

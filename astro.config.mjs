95aac0092412dcc5e42d6b5af7f676a3cce1ac53
Chris Lansing <cslansin@gmail.com>
2024-03-08 14:57:38 -0500

    update astro config output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: cloudflare({
-    runtime: {
-      mode: "local"
-    }
-  }),
-  integrations: [tailwind()]
+  output: "hybrid",
+  adapter: cloudflare(),
+  integrations: [tailwind()],
 });

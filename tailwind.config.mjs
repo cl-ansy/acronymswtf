70a79fde332afffd9295a25e51a0c8108a13e945
Chris Lansing <cslansin@gmail.com>
2024-03-08 14:38:30 -0500

    add color scheme and basic acronym search

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,14 +2,26 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				headers: ["Josefin Sans Variable"],
-				sans: ["Merriweather Sans Variable", ...defaultTheme.fontFamily.sans],
-			},
-		},
-	},
-	plugins: [require('@tailwindcss/typography')],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#050401",
+        foreground: "#F1FAEE",
+        primary: "#00B4D8",
+        secondary: "#303036",
+        accent: "#C9C699",
+        destructive: "#FC5130",
+        "primary-foreground": "#050401",
+        "secondary-foreground": "#F1FAEE",
+        "accent-foreground": "#050401",
+        "destructive-foreground": "#050401",
+      },
+      fontFamily: {
+        headers: ["Josefin Sans Variable"],
+        sans: ["Merriweather Sans Variable", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
